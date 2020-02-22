1fdeefdc0a67f2d163c118ab39385ce07fa23637
alreadyExisted <vit.polyanskiy@gmail.com>
2020-02-22 16:22:36 +0200

    Change reset sw cache

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,6 @@
-exports.onServiceWorkerUpdateReady = () => window.location.reload(true)
+export const onServiceWorkerUpdateReady = () => {
+  const answer = window.confirm(`Этот сайт обновился. Перезагрузить страницу?`)
+  if (answer === true) {
+    window.location.reload()
+  }
+}
